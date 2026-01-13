e614502c23ac6ef9512a9b06d3f01c6dc89f0f6d
Nikesh Chaudhary <Nikesh.chaudhary@block-synergy.com>
2026-01-14 03:05:23 +0530

    error

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello from Jenkins! 🚀</h1><p>Express app is running.</p><p>These are new modifications.</p>');
+  res.send('<h1>Hello from Jenkins! 🚀</h1><p>Express app is running.</p>
+    <p>These are new modifications.</p>');
 });
 
 app.get('/status', (req, res) => {
