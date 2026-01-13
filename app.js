4c65899934549cb1fa555ee925ccec3827c066da
Nikesh Chaudhary <Nikesh.chaudhary@block-synergy.com>
2026-01-14 03:13:26 +0530

    error fixed

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('<h1>Hello from Jenkins! 🚀</h1><p>Express app is running.</p>
-    <p>These are new modifications.</p>');
+app.get("/", (req, res) => {
+  res.send(`<h1>Hello from Jenkins! 🚀</h1>
+    <p>Express app is running.</p>
+    <p>These are new modifications.</p>`);
 });
 
-app.get('/status', (req, res) => {
-  res.status(200).json({ status: 'UP', timestamp: new Date() });
+app.get("/status", (req, res) => {
+  res.status(200).json({ status: "UP", timestamp: new Date() });
 });
 
 const server = app.listen(port, () => {
